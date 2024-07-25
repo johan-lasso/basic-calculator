17dc6b07e8b2416f44cf20190a3e097359601030
johan-lasso <lassojohanrendon@gmail.com>
2024-07-25 16:55:52 -0500

    function to print result
    Add function to print operation result for code reuse

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <cmath>
 
+// Function that does not return anything 
+void printResult(double a, double b, double result, char symbol) {
+    std::cout << a << " " << symbol << " " << b << " = " << result << "\n";
+}
+
 int main() {
     double valOne, valTwo, result; // double stores decimal numbers
     char opSymbol; // char stores characters
@@ -17,23 +22,23 @@ int main() {
     switch (opSymbol) {
     case '+':
         result = valOne + valTwo;
-        std::cout << valOne << " + " << valTwo << " = " << result << "\n";
+        printResult(valOne, valTwo, result, '+'); // Calling a function
         break;
     case '-':
         result = valOne - valTwo;
-        std::cout << valOne << " - " << valTwo << " = " << result << "\n";
+        printResult(valOne, valTwo, result, '-');
         break;
     case '*':
         result = valOne * valTwo;
-        std::cout << valOne << " * " << valTwo << " = " << result << "\n";
+        printResult(valOne, valTwo, result, '*');
         break;
     case '/':
         result = valOne / valTwo;
-        std::cout << valOne << " / " << valTwo << " = " << result << "\n";
+        printResult(valOne, valTwo, result, '/');
         break;
     case '^':
         result = pow(valOne, valTwo);
-        std::cout << valOne << " ^ " << valTwo << " = " << result << "\n";
+        printResult(valOne, valTwo, result, '^');
         break;
     default: // In case any of the previous options were true
         std::cout << "invalid operator\n";
